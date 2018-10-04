95a735dbf690e0bf9a6abc15468f252ec3260dc0
Daniel Nazeri <danielnazeri@yahoo.com>
2018-10-04 14:31:30 -0700

    style addition, html routes update, server fix, views large addition

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,16 +1,15 @@
 var db = require("../models");
-
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Pet.findAll({}).then(function(dbExamples) {
+    // db.Pet.findAll({}).then(function(dbExamples) {
+    //   res.render("home", {
+    //     msg: "Welcome!",
+    //     pets: dbExamples
+    //   });
       res.render("index", {
-        msg: "Welcome!",
-        pets: dbExamples
-      });
     });
   });
-
   // Load example page and pass in an example by id
   app.get("/pet/:id", function(req, res) {
     db.Pet.findOne({ where: { id: req.params.id } }).then(function(dbPet) {
@@ -19,7 +18,18 @@ module.exports = function(app) {
       });
     });
   });
-
+  app.get("/signup", function(req, res) {
+    console.log("hello from signup");
+    res.render("signup");
+  });
+  app.get("/provider", function(req, res){
+    console.log("provider");
+    res.render("provider");
+  })
+  app.get("/borrower", function(req, res){
+    console.log("borrower");
+    res.render("borrower");
+  })
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
